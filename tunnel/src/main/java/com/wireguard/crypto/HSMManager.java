468bb5a2683106a4ac3860be8d1cb45238f41a7e
mike111-droid <micha.j.eyl@gmail.com>
2022-03-16 11:49:21 +0100

    Add hardware-backed component and Ratchet classes

--- tunnel/src/main/java/com/wireguard/crypto/HSMManager.java
+++ tunnel/src/main/java/com/wireguard/crypto/HSMManager.java
@@ -1,6 +1,9 @@
 /*
  * Copyright © 2017-2022 WireGuard LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * This class is supposed to manage the SmartCard-HSM by CardContact Systems GmbH.
+ * It can take an initial value and return a PSK in Base64 as String.
  */
 
 package com.wireguard.crypto;
---
+++ tunnel/src/main/java/com/wireguard/crypto/RatchetManager.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2017-2022 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This class is supposed to perform the Ratchet Operation (SHA256) on a given String and return the new PSK
+ * in Base64 as String.
+ */
+
+package com.wireguard.crypto;
+
+public class RatchetManager {
+}
