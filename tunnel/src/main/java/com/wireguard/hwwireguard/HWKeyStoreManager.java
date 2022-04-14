81388b1b2005dfc7d03edc14f5e69f878cf5bbcb
mike111-droid <micha.j.eyl@gmail.com>
2022-04-14 12:33:19 +0200

    Joined keyStoreOperation for RSA and AES

--- tunnel/src/main/java/com/wireguard/hwwireguard/HWKeyStoreManager.java
+++ tunnel/src/main/java/com/wireguard/hwwireguard/HWKeyStoreManager.java
@@ -420,7 +420,54 @@ public class HWKeyStoreManager {
         }
     }
 
-    // TODO: combine to one function keyStoreOperation
+    // TODO: clean up catch clause
+
+    /**
+     * Function to perform the AndroidKeyStore operation (AESECB or RSA).
+     *
+     * @param keyType: Allowed AES or RSA. AES leads to AESECB.
+     * @param alias  : Alias of key to use.
+     * @param init   : Input to sign or encrypt.
+     * @return       : Key for newPSK.
+     */
+    public Key keyStoreOperation(KeyType keyType, String alias, String init) {
+        KeyStore keyStore;
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+            byte[] initBytes = init.getBytes("UTF8");
+
+            if(keyType == KeyType.AES) {
+                java.security.Key key = keyStore.getKey(alias, null);
+
+                /* Prepare cipher */
+                Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+                cipher.init(Cipher.ENCRYPT_MODE, key);
+
+                /* Encrypt init with AES_CBC*/
+                MessageDigest sha256 = MessageDigest.getInstance("SHA256");
+                sha256.update(initBytes);
+                byte[] digestBytes = sha256.digest();
+                byte[] pskBytes = cipher.doFinal(digestBytes);
+
+                /* Return new PSK */
+                return Key.fromBase64(Base64.encodeToString(pskBytes, Base64.DEFAULT));
+            }else if(keyType == KeyType.RSA) {
+                KeyStore.Entry entry = keyStore.getEntry(alias, null);
+
+                PrivateKey privKey = ((KeyStore.PrivateKeyEntry) entry).getPrivateKey();
+                Signature sig = Signature.getInstance("SHA256WithRSA");
+                sig.initSign(privKey);
+                sig.update(initBytes);
+                return Key.fromBase64(Base64.encodeToString(sig.sign(), Base64.DEFAULT));
+            }else{
+                throw new NoSuchAlgorithmException("This function only allows RSA and AES (AESECB)...");
+            }
+        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | KeyFormatException | UnrecoverableEntryException | SignatureException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            return null;
+        }
+    }
 
     /**
      * Function to return key with init as initial value (Using AES CBC).
@@ -454,67 +501,6 @@ public class HWKeyStoreManager {
             /* Return new PSK */
             return Key.fromBase64(Base64.encodeToString(pskBytes, Base64.DEFAULT));
         } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | KeyFormatException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-            return null;
-        }
-    }
-
-    /**
-     * Function to return key with init as initial value (Using AES ECB).
-     *
-     * @param alias: String with alias of key from AndroidKeyStore that should be used.
-     * @param  init: String with init value.
-     * @return     : Key that can be used as new PSK.
-     */
-    public Key keyStoreOperationAESECB(String alias, String init) {
-        KeyStore keystore;
-        try {
-            /* Get correct key from AndroidKeyStore */
-            keystore = KeyStore.getInstance("AndroidKeyStore");
-            keystore.load(null);
-            java.security.Key key = keystore.getKey(alias, null);
-
-            /* Prepare cipher */
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-
-            /* Encrypt init with AES_CBC*/
-            byte[] initBytes = init.getBytes("UTF-8");
-            MessageDigest sha256 = MessageDigest.getInstance("SHA256");
-            sha256.update(initBytes);
-            byte[] digestBytes = sha256.digest();
-            byte[] pskBytes = cipher.doFinal(digestBytes);
-
-            /* Return new PSK */
-            return Key.fromBase64(Base64.encodeToString(pskBytes, Base64.DEFAULT));
-        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException | NoSuchPaddingException | BadPaddingException | IllegalBlockSizeException | KeyFormatException | InvalidKeyException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-            return null;
-        }
-    }
-
-    /**
-     * Function to return key with init as initial value (using RSA Signatur).
-     *
-     * @param alias: String with alias of key from AndroidKeyStore that should be used.
-     * @param  init: String with init value.
-     * @return     : Key that can be used as new PSK.
-     */
-    public Key keyStoreOperationRSA(String alias, String init) {
-        KeyStore keyStore = null;
-        Key key = null;
-        try {
-            keyStore = KeyStore.getInstance("AndroidKeyStore");
-            keyStore.load(null);
-            KeyStore.Entry entry = keyStore.getEntry(alias, null);
-            PrivateKey privKey = ((KeyStore.PrivateKeyEntry) entry).getPrivateKey();
-            byte[] data = init.getBytes("UTF8");
-            Signature sig = Signature.getInstance("SHA256WithRSA");
-            sig.initSign(privKey);
-            sig.update(data);
-            byte[] pskBytes = sig.sign();
-            return Key.fromBase64(Base64.encodeToString(pskBytes, Base64.DEFAULT));
-        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException | UnrecoverableEntryException | InvalidKeyException | SignatureException | KeyFormatException e) {
             Log.e(TAG, Log.getStackTraceString(e));
             return null;
         }
